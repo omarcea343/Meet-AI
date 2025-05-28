5b3165604853dfa56e6ab68eceb0fe3de9420c36
Omar Zuniga <miguel_omar30@hotmail.com>
2025-05-28 13:21:09 -0600

    agents setup

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -1,19 +1,22 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
+
 import { useTRPC } from "@/trpc/client";
+import { LoadingState } from "@/components/loading-state";
+import { ErrorState } from "@/components/error-state";
 
 export const AgentsView = () => {
 	const trpc = useTRPC();
-	const { data, isLoading, isError } = useQuery(trpc.agents.getMany.queryOptions());
-
-	if (isLoading) {
-		return <div>Loading...</div>;
-	}
-
-	if (isError) {
-		return <div>Error!</div>;
-	}
+	const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
 
 	return <div>{JSON.stringify(data, null, 2)}</div>;
+};
+
+export const AgentsViewLoading = () => {
+	return <LoadingState title="Loading Agents" description="Please wait this may take a few seconds..." />;
+};
+
+export const AgentsViewError = () => {
+	return <ErrorState title="Error Loading Agents" description="Something went wrong, please try again later." />;
 };
