b19cc6471540ae47a0279d4416ffcb189c4e5e47
Omar Zuniga <miguel_omar30@hotmail.com>
2025-05-28 13:42:26 -0600

    Fix

--- src/components/error-state.tsx
+++ src/components/error-state.tsx
@@ -1,11 +1,11 @@
 import { AlertCircleIcon } from "lucide-react";
 
-interface Props {
+interface ErrorStateProps {
 	title: string;
 	description: string;
 }
 
-export const ErrorState = ({ title, description }: Props) => {
+export const ErrorState = ({ title, description }: ErrorStateProps) => {
 	return (
 		<div className="py-4 px-8 flex flex-1 items-center justify-center">
 			<div className="flex flex-col items-center justify-center gap-y-6 bg-background rounded-lg p-10 shadow-sm">
