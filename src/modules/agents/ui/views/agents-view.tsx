ba49ce39e5964bd2b06bc3572a02b62d67899f4a
Omar Zuniga <miguel_omar30@hotmail.com>
2025-05-29 22:20:04 -0600

    Responsive Dialog

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -2,9 +2,9 @@
 
 import { useSuspenseQuery } from "@tanstack/react-query";
 
-import { useTRPC } from "@/trpc/client";
-import { LoadingState } from "@/components/loading-state";
 import { ErrorState } from "@/components/error-state";
+import { LoadingState } from "@/components/loading-state";
+import { useTRPC } from "@/trpc/client";
 
 export const AgentsView = () => {
 	const trpc = useTRPC();
