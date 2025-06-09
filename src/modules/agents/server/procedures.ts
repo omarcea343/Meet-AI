4130318a9398b1cb7d817d8bdc90255fb9b8e0ec
Omar Zuniga <miguel_omar30@hotmail.com>
2025-06-09 13:05:37 -0600

    Agent PAge

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -5,10 +5,11 @@ import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@
 import { db } from "@/db";
 import { agents } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import { TRPCError } from "@trpc/server";
 import { agentsInsertSchema } from "../schemas";
 
 export const agentsRouter = createTRPCRouter({
-	getOne: protectedProcedure.input(z.object({ id: z.string() })).query(async ({ input }) => {
+	getOne: protectedProcedure.input(z.object({ id: z.string() })).query(async ({ input, ctx }) => {
 		const [existingAgent] = await db
 			.select({
 				//TODO: change to actual count
@@ -16,8 +17,11 @@ export const agentsRouter = createTRPCRouter({
 				...getTableColumns(agents),
 			})
 			.from(agents)
-			.where(eq(agents.id, input.id));
+			.where(and(eq(agents.id, input.id), eq(agents.userId, ctx.auth.user.id)));
 
+		if (!existingAgent) {
+			throw new TRPCError({ code: "NOT_FOUND", message: "Agent not found" });
+		}
 		return existingAgent;
 	}),
 	getMany: protectedProcedure
